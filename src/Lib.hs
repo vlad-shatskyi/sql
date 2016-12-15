dfb49df3d645dd9abd4199fa47ee0f65a173f45f
Volodymyr Shatskyi <shockone89@gmail.com>
2016-12-15 16:22:58 +0200

    Make table and column classes.

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,18 +4,25 @@ module Lib
 
 import Data.List (intercalate)
 import Data.Function ((&))
+import Data.Char (toLower)
 
 someFunc :: IO ()
-someFunc = putStrLn $ select [Column "name"] (Table "users")
+someFunc = putStrLn $ select [Name] Users
 
-select :: [Column] -> Table -> String
+select :: (Column column, Table table) => [column] -> table -> String
 select columns table = sql
   where sql = [ "SELECT " ++ columnsSQL
               , "FROM " ++ tableName
               ] & intercalate "\n"
         columnsSQL = intercalate ", " (map columnName columns)
-        columnName (Column name) = name
-        tableName  = let (Table name) = table in name
+        columnName = map toLower . show
+        tableName  = map toLower $ show table
 
-data Table  = Table String
-data Column = Column String
+class Show a => Table a
+class Show a => Column a
+
+data Users = Users deriving Show
+data Name = Name deriving Show
+
+instance Table Users
+instance Column Name
