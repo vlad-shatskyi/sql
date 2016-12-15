6ab63e28e2378967217846058a5f1e54f109fa82
Volodymyr Shatskyi <shockone89@gmail.com>
2016-12-15 16:06:25 +0200

    Simplest query.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,20 @@ module Lib
     ( someFunc
     ) where
 
+import Data.List (intercalate)
+import Data.Function ((&))
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn $ select [Column "name"] (Table "users")
+
+select :: [Column] -> Table -> String
+select columns table = sql
+  where sql = [ "SELECT " ++ columnsSQL
+              , "FROM " ++ tableName
+              ] & intercalate "\n"
+        columnsSQL = intercalate ", " (map columnName columns)
+        columnName (Column name) = name
+        tableName  = let (Table name) = table in name
+
+data Table  = Table String
+data Column = Column String
